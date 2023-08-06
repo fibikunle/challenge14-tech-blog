152b1aff81ca772b1218ecb139fe969dd85dbf93
fibikunle <43515489+fibikunle@users.noreply.github.com>
2023-08-06 10:56:20 -0500

    added cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-
+const cors = require('cors')
 // helper function
 const helpers = require('./utils/helpers');
 
@@ -15,7 +15,7 @@ const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001
-
+app.use(cors())
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
